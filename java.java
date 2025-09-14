fabd67d5ade7bcec4b7bd1659df3439405c3d8a1
Sofi167 <SmartSofi@yandex.ru>
2025-09-14 17:36:41 +0300

    Update java.java

--- java.java
+++ java.java
@@ -1,1 +1,13 @@
+import java.util.ArrayList;
 
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<>();
+        
+        list.add("Привет");
+        list.add("Мир");
+        
+        System.out.println(list); // [Привет, Мир]
+        System.out.println(list.get(1)); // Мир
+    }
+}
