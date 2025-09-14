b405a77a0bfdd49f2b81c4ee255d529be289119e
Sofi167 <SmartSofi@yandex.ru>
2025-09-14 17:39:10 +0300

    Update c++.cpp

--- c++.cpp
+++ c++.cpp
@@ -1,1 +1,14 @@
+#include <iostream>
+#include <vector>
+using namespace std;
 
+int main() {
+    vector<int> list;
+    
+    list.push_back(1);
+    list.push_back(2);
+    list.push_back(3);
+    
+    
+    return 0;
+}
