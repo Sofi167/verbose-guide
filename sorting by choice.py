0350eaeda778f87ac3d0f857d04657e87a0171d9
Sofi167 <SmartSofi@yandex.ru>
2025-10-31 21:38:16 +0300

    Update sorting by choice.py

--- sorting by choice.py
+++ sorting by choice.py
@@ -1,18 +1,1 @@
-def selection_sort(arr):
-    n = len(arr)
-    for i in range(n):
-        # Ищем минимальный элемент в неотсортированной части массива
-        min_index = i
-        for j in range(i + 1, n):
-            if arr[j] < arr[min_index]:
-                min_index = j
-        # Меняем минимальный элемент с первым элементом неотсортированной части
-        arr[i], arr[min_index] = arr[min_index], arr[i]
-    return arr
-
-# Пример использования
-arr = [64, 25, 12, 22, 11]
-sorted_arr = selection_sort(arr)
-print("Отсортированный массив:", sorted_arr)
-
-вывод программы: Отсортированный массив: [11, 12, 22, 25, 64]
+смотрите файл sorting by choice.py
